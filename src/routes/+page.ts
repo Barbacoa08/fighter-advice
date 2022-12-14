7057a78ade987551ce041a7d7ed6d4bba70d8113
barbajoe <sabe12070@gmail.com>
2022-12-14 16:16:01 -0600

    add SvelteKit typing and grab data from an openapi

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,9 +1,15 @@
+import type { PageLoad } from "./$types";
+
 export interface PageData {
   user: string;
 }
 
-export const load = () => {
+export const load: PageLoad = async ({ fetch }) => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users/1");
+  const user = await res.json();
+  console.log(user);
+
   return {
-    user: "Nak Muay",
+    user: user.username || "Nak Muay",
   } satisfies PageData;
 };
