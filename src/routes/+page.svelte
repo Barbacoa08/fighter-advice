14a656dd5bed02eaabd04d491a2dc932c5568530
barbajoe <sabe12070@gmail.com>
2022-12-14 13:44:03 -0600

    use my default prettier settings
    tab === 2 spaces
    ending comma: always
    line width 80ch
    ect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <svelte:head>
-	<title>Fighter Advice</title>
-	<meta name="description" content="Svelte demo app" />
+  <title>Fighter Advice</title>
+  <meta name="description" content="Svelte demo app" />
 </svelte:head>
 
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
