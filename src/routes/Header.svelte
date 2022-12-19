0f2596a98a2cf4333237926411ebbd28f0e46517
barbajoe <sabe12070@gmail.com>
2022-12-19 15:58:37 -0600

    extract Header and Footer components

---
+++ src/routes/Header.svelte
@@ -0,0 +1,35 @@
+<header>
+  <h1 class="text-gradient">Fighter Advice</h1>
+</header>
+
+<style>
+  :root {
+    --image-gradient: linear-gradient(0deg, #4f39fa, #da62c4);
+  }
+  header h1 {
+    margin-bottom: 0;
+    text-align: center;
+  }
+
+  .text-gradient {
+    font-weight: 900;
+    background-image: var(--image-gradient);
+    -webkit-background-clip: text;
+    background-clip: text;
+    -webkit-text-fill-color: transparent;
+    background-size: 100% 200%;
+    background-position-y: 100%;
+    border-radius: 0.4rem;
+    animation: pulse 4s ease-in-out infinite;
+  }
+
+  @keyframes pulse {
+    0%,
+    100% {
+      background-position-y: 0%;
+    }
+    50% {
+      background-position-y: 80%;
+    }
+  }
+</style>
