e58b2982ef002d24b5cf0e344e4eaf99c19f389a
barbajoe <sabe12070@gmail.com>
2022-12-20 12:06:16 -0600

    testing environment variables (#14)
    * `$app/environment` is cooooool
    
    * add environment variable examples
    
    * move from `@sveltejs/adapter-auto` to `@sveltejs/adapter-netlify`

--- src/routes/user/+page.svelte
+++ src/routes/user/+page.svelte
@@ -1,3 +1,10 @@
+<script lang="ts">
+  import { PUBLIC_API_KEY, PUBLIC_SECRECT_STRING } from "$env/static/public";
+
+  console.info("PUBLIC_API_KEY: ", PUBLIC_API_KEY);
+  console.info("PUBLIC_SECRECT_STRING: ", PUBLIC_SECRECT_STRING);
+</script>
+
 <div>Hello "allowed" User!</div>
 
 <p>Return <a href="/">home</a>?</p>
