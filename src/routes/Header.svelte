89f45f82254bd8d0e94eda61ab3185cadb79ee69
barbajoe <sabe12070@gmail.com>
2022-12-23 16:39:53 -0600

    better header

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-  import { Header, HeaderNav, HeaderNavItem } from "agnostic-svelte";
-
   import { page } from "$app/stores";
   import { Logo } from "$lib";
 
@@ -8,21 +6,65 @@
   $: isAbout = $page.url.pathname === "/about";
 </script>
 
-<Header>
-  <div slot="logoleft"><Logo /></div>
+<header>
+  <Logo />
 
-  <HeaderNav>
-    <HeaderNavItem>
-      <a href="/" class:active={isHome}>Home</a>
-    </HeaderNavItem>
-    <HeaderNavItem>
-      <a href="/about" class:active={isAbout}>About</a>
-    </HeaderNavItem>
-  </HeaderNav>
-</Header>
+  <ul>
+    <li class:active={isHome}>
+      <a href="/">Home</a>
+    </li>
+
+    <li class:active={isAbout}>
+      <a href="/about">About</a>
+    </li>
+  </ul>
+</header>
 
 <style>
-  .active {
+  :root {
+    --header-background-color: white;
+    --color-link-text-active: #0000ee;
+  }
+  @media (prefers-color-scheme: dark) {
+    :root {
+      --header-background-color: #1a202c;
+      --color-link-text-active: #87e7ee;
+    }
+  }
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    background-color: var(--header-background-color);
+    box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.5);
+    border-bottom: 1px solid rgba(255 255 255 / 6%);
+    padding: 0.5rem 1.5rem;
+  }
+
+  header ul {
+    display: flex;
+    list-style: none;
+    margin: 0;
+  }
+
+  header ul li {
+    padding: 0.5rem;
+    border-bottom: 2px solid transparent;
+    color: var(--color-link-text-active);
+  }
+  header ul li.active {
+    padding: 0.5rem;
+    border-bottom: 2px solid;
+    color: var(--color-link-text-active);
+  }
+
+  header ul li a,
+  header ul li.active a {
+    text-decoration-line: none;
+  }
+  header ul li.active a {
     color: var(--color-link-text-active);
   }
 </style>
