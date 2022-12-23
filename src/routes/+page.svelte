b4380d838c331e48e920a49dfd46c371845a7646
barbajoe <sabe12070@gmail.com>
2022-12-23 16:55:17 -0600

    create a better header (#17)
    * remove stuff I'm not planning on using
    
    * add a `Logo` component and add it to the `Header`
    
    * more flexible `Logo`
    
    * `<a>` are not the same color always
    
    wether they are `:visited` or not
    
    * better header
    
    * cleanup
    
    * cleanup
    
    * resolve a11y landmark issue

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,48 +1,34 @@
-<script lang="ts">
-  import { browser, dev } from "$app/environment";
-  import SetUsername from "$lib/SetUsername.svelte";
-  import type { PageData } from "./+page.server";
-
-  export let data: PageData;
-
-  $: ({
-    user: { username },
-  } = data);
-</script>
-
 <svelte:head>
   <title>Fighter Advice</title>
   <meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<h2>
-  Welcome to BarbaJoe's Fighter Advice:
-  <span class="username">{username}</span>
-</h2>
-
-<SetUsername {username} />
+<h1>Welcome to barbajoe's <span class="text-gradient">Fighter Advice</span></h1>
 
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
 
-<h3>Routing test</h3>
-
-<ul>
-  <li>Go to the <a href="/about">About page</a></li>
-  {#if dev && browser}
-    <li>Go "allowed" <a href="/user?allowed=true">User page</a></li>
-    <li>
-      Be redirect to homepage after attempting to go to
-      <a href="/user">User page</a> when not "allowed"
-    </li>
-    <li>Go to the <a href="/not-found">Not Found page</a></li>
-    <li>Go to the <a href="/fake-form">Fake Form page</a></li>
-  {/if}
-</ul>
-
 <style>
-  span.username {
-    color: red;
+  .text-gradient {
+    font-weight: 900;
+    background-image: linear-gradient(0deg, #4f39fa, #da62c4);
+    -webkit-background-clip: text;
+    background-clip: text;
+    -webkit-text-fill-color: transparent;
+    background-size: 100% 200%;
+    background-position-y: 100%;
+    border-radius: 0.4rem;
+    animation: pulse 4s ease-in-out infinite;
+  }
+
+  @keyframes pulse {
+    0%,
+    100% {
+      background-position-y: 0%;
+    }
+    50% {
+      background-position-y: 80%;
+    }
   }
 </style>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,35 +1,70 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { Logo } from "$lib";
+
+  $: isHome = $page.url.pathname === "/";
+  $: isAbout = $page.url.pathname === "/about";
+</script>
+
 <header>
-  <h1 class="text-gradient">Fighter Advice</h1>
+  <Logo />
+
+  <ul>
+    <li class:active={isHome}>
+      <a href="/">Home</a>
+    </li>
+
+    <li class:active={isAbout}>
+      <a href="/about">About</a>
+    </li>
+  </ul>
 </header>
 
 <style>
   :root {
-    --image-gradient: linear-gradient(0deg, #4f39fa, #da62c4);
+    --header-background-color: white;
+    --color-link-text-active: #0000ee;
   }
-  header h1 {
-    margin-bottom: 0;
-    text-align: center;
+  @media (prefers-color-scheme: dark) {
+    :root {
+      --header-background-color: #1a202c;
+      --color-link-text-active: #87e7ee;
+    }
   }
 
-  .text-gradient {
-    font-weight: 900;
-    background-image: var(--image-gradient);
-    -webkit-background-clip: text;
-    background-clip: text;
-    -webkit-text-fill-color: transparent;
-    background-size: 100% 200%;
-    background-position-y: 100%;
-    border-radius: 0.4rem;
-    animation: pulse 4s ease-in-out infinite;
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    background-color: var(--header-background-color);
+    box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.5);
+    border-bottom: 1px solid rgba(255 255 255 / 6%);
+    padding: 0.5rem 1.5rem;
   }
 
-  @keyframes pulse {
-    0%,
-    100% {
-      background-position-y: 0%;
-    }
-    50% {
-      background-position-y: 80%;
-    }
+  header ul {
+    display: flex;
+    list-style: none;
+    margin: 0;
+  }
+
+  header ul li {
+    padding: 0.5rem;
+    border-bottom: 2px solid transparent;
+    color: var(--color-link-text-active);
+  }
+  header ul li.active {
+    padding: 0.5rem;
+    border-bottom: 2px solid;
+    color: var(--color-link-text-active);
+  }
+
+  header ul li a,
+  header ul li.active a {
+    text-decoration-line: none;
+  }
+  header ul li.active a {
+    color: var(--color-link-text-active);
   }
 </style>
