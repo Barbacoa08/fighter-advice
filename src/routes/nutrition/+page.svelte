cb54ae044c0304af5c58a67cd9d3078ca213167b
barbajoe <sabe12070@gmail.com>
2022-12-24 13:07:57 -0600

    get some data in there

--- src/routes/nutrition/+page.svelte
+++ src/routes/nutrition/+page.svelte
@@ -9,4 +9,96 @@
 
 <h1>{header}</h1>
 
-<p>Under Construction</p>
+<p>"Abs are made in the gym"</p>
+
+<p>
+  This phrase is more true than most people think. In a fight you want to be at
+  your absolute best shape possible, and if you haven't been eating well, it
+  <i>will</i> negatively affect your performance.
+</p>
+
+<p>
+  With that in mind, let's get into how to eat to fule your best physical shape
+  possible!
+</p>
+
+<h2>What to Eat</h2>
+<p>
+  There is no one diet that is best for everyone. Every body is different, and
+  best thing you can do is to experiment and see what works best for you.
+</p>
+
+<p>
+  Experiment to find what works for you and then <i>stick with it</i> to see maxiumum
+  effect
+</p>
+
+<h2>A collection of resources:</h2>
+<ul>
+  <li>
+    <a href="https://www.webmd.com/diet/obesity/features/6_meals_a_day">
+      6 Meals a Day for Weight Loss
+    </a>
+  </li>
+  <li>
+    <ul>
+      <li>Many meals a day <i>does not</i> increase your metabolism</li>
+      <li>
+        Many meals a day <i>can</i> help you control cravings and appetite
+      </li>
+      <li>High fiber foods help you <i>feel</i> full for longer</li>
+    </ul>
+  </li>
+
+  <li>First time fighters</li>
+  <li>
+    <ul>
+      <li>
+        <i>Do not</i> cut more than 5lbs
+      </li>
+
+      <li>
+        It's an enormous stressor on your body, and you need to ease into it to
+        find where your performance maximum is
+      </li>
+    </ul>
+  </li>
+
+  <li>Amateur fighters</li>
+  <li>
+    <ul>
+      <li>Don't cut more than 8% of your body weight</li>
+
+      <li>
+        Without a professional nutritionist, cutting more than 8% is dangerous,
+        especially if you are already lean
+      </li>
+    </ul>
+  </li>
+
+  <li>No alcohol three weeks out</li>
+
+  <li>
+    <a href="https://www.kachava.com/">Ka'Chava</a> is my favorite post-workout
+  </li>
+</ul>
+
+<!--
+  - [Fight Week Weight Cut Manual](https://drive.google.com/file/d/1y43TXbQSIMSszrYNAlToa68dzGUouU2O/view)
+    - Lots of “DO NOT” (unsafe) methods
+    - Lots of “DO” (safe) methods
+    - Advice on foods to eat directly after weigh in
+    - Advice on foods and meals for healthy week-of eating
+- [Intermittent Fasting (16|8 strategy)](https://www.healthline.com/nutrition/16-8-intermittent-fasting#basics)
+    - Once or twice a week, fast for 16hrs, eat for 8hrs
+        - For those 16hrs, only zero-calorie drinks (water, tea, plain coffee)
+    - Several links to studies showing IF to be safe and effective
+        - Especially for controlling cravings and blood sugar levels
+    - UNSAFE for some people
+        - Diabetics, postmenopausal, pregnant, those wishing to become pregnant
+- [Diet-boosting foods](https://medlineplus.gov/ency/patientinstructions/000810.htm)
+    - Lots of links to studies showing what foods are ideal for individual goals
+- [Don Heatrick](https://heatrick.com/)
+    - Just an enormous amount of information for training, weight managment, motivation, and more
+    - He also has a youtube channel, and [this video on Discipline](https://www.youtube.com/watch?v=mVI8Ff-J-sc) is particularly grea
+-->
