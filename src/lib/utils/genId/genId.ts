309c65083820507e37f7517729bbf61364137128
barbajoe <sabe12070@gmail.com>
2022-12-29 15:06:01 -0600

    add `genId` util method

---
+++ src/lib/utils/genId/genId.ts
@@ -0,0 +1,18 @@
+// stolen from stackoverflow: https://stackoverflow.com/a/70307091/1022765
+type Enumerate<
+  N extends number,
+  Acc extends number[] = []
+> = Acc["length"] extends N
+  ? Acc[number]
+  : Enumerate<N, [...Acc, Acc["length"]]>;
+
+type Range<F extends number, T extends number> = Exclude<
+  Enumerate<T>,
+  Enumerate<F>
+>;
+
+export const genId = (length: Range<2, 36> = 8) => {
+  return Math.random()
+    .toString(36)
+    .substring(2, length + 2);
+};
