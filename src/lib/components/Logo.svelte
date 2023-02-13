09432471fc92199ec4495258d9b25032d09759f2
barbajoe <sabe12070@gmail.com>
2023-02-13 17:44:19 -0600

    resolve Lighthouse warning

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
   import logoBlack from "$lib/assets/mongkong-logo-black.webp";
   import logoWhite from "$lib/assets/mongkong-logo-white.webp";
-
-  export let width = 50;
 </script>
 
 <picture>
   <source media="(prefers-color-scheme: dark)" srcset={logoWhite} />
-  <img {width} src={logoBlack} alt="Mongkong Logo" />
+  <img width="50" height="50" src={logoBlack} alt="Mongkong Logo" />
 </picture>
