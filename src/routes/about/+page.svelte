b00b1e18110f6fcf8c8aeed1cdf76677a257e48a
barbajoe <sabe12070@gmail.com>
2023-02-17 15:08:44 -0800

    add discord server invite link to About page (#63)
    * add discord server invite link to About page
    
    * add what to do when the invite limit is reached

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -13,7 +13,7 @@
   As a Nak Muay, there are a <strong>ton</strong> or amazing resources out there!
   But I've found it difficult to keep all of those resources organized and easy to
   access. So I decided to create Fighter Advice as a central place to store all of
-  my favorite resources.
+  my favorite resources and share my own findings.
 </p>
 
 <p>I hope you find Fighter Advice useful!</p>
@@ -25,4 +25,16 @@
   >
     visit this link
   </Link> and click "New Issue".
+</p>
+
+<p>
+  You can also <Link isExternal href="https://discord.gg/V3HhJkP7mG">
+    join the Fighter Advice Discord
+  </Link> if you'd like to talk to me or other users. If the invite link is expired,
+  please <Link
+    href="https://github.com/Barbacoa08/fighter-advice/issues"
+    isExternal
+  >
+    create an issue
+  </Link>.
 </p>
