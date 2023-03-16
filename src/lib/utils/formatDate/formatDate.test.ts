0a93ad8f83e3cd7331143c1225c6efe1bc43f96d
barbajoe <sabe12070@gmail.com>
2023-03-16 16:41:31 -0500

    fix tests

--- src/lib/utils/formatDate/formatDate.test.ts
+++ src/lib/utils/formatDate/formatDate.test.ts
@@ -1,28 +1,23 @@
 import { formatDate } from "./formatDate";
 
 describe("formatDate", () => {
+  // IMPORTANT NOTE: skipping tests that could fail due to unspecified timezones
+  // eg, "2020-01-01" could translate to "Dec 31, 2019" or "Jan 1, 2020"
+
   it("should format a date", () => {
-    const date = new Date("2020-01-01");
+    const date = new Date("2023-02-11T23:53:48.267Z");
     const formattedDate = formatDate(date);
-    expect(formattedDate).toBe("Dec 31, 2019"); // conversion to UTC
+    expect(formattedDate).toBe("Feb 11, 2023");
   });
 
   it("should format a date string", () => {
-    const date = "2020-01-01";
-    const formattedDate = formatDate(date);
-    expect(formattedDate).toBe("Dec 31, 2019"); // conversion to UTC
-
-    const date2 = "2023-02-11T23:53:48.267Z";
-    const formattedDate2 = formatDate(date2);
+    const date1 = "2023-02-11T23:53:48.267Z";
+    const formattedDate2 = formatDate(date1);
     expect(formattedDate2).toBe("Feb 11, 2023");
 
-    const date3 = "2023-02-11T23:53:48.267+07:00";
-    const formattedDate3 = formatDate(date3);
+    const date2 = "2023-02-11T23:53:48.267+07:00";
+    const formattedDate3 = formatDate(date2);
     expect(formattedDate3).toBe("Feb 11, 2023");
-
-    const date4 = "2023-02-11";
-    const formattedDate4 = formatDate(date4);
-    expect(formattedDate4).toBe("Feb 10, 2023"); // conversion to UTC
   });
 
   it("should return undefined if no date is provided", () => {
